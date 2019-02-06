ac9c669f2a0ace4c3b457f239a8cfe254dc5b069
Kim Duclos <kimduclos@gmail.com>
2019-02-06 14:15:58 -0700

    dom events working on icons

---
+++ Js/index.js
@@ -0,0 +1,15 @@
+// DOM event to change contact icons on mouse hover
+let changeIconColor = document.querySelector('.fa-envelope-open');
+changeIconColor.addEventListener('mouseover', e => {
+changeIconColor.style.color = "orange";
+});
+
+let changeIconColor2 = document.querySelector('.fa-mobile-alt');
+changeIconColor2.addEventListener('mouseover', e => {
+changeIconColor2.style.color = "orange";
+});
+
+let changeIconColor3 = document.querySelector('.fa-map-marker-alt');
+changeIconColor3.addEventListener('mouseover', e => {
+changeIconColor3.style.color = "orange";
+});
