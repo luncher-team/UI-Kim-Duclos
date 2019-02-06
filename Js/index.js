737c4414ffc7ae83625aa297fd28aa4253bd4da9
Kim Duclos <kimduclos@gmail.com>
2019-02-06 14:54:08 -0700

    added some details, still trying to get a slideshow of objects working

--- Js/index.js
+++ Js/index.js
@@ -1,15 +1,60 @@
-// DOM event to change contact icons on mouse hover
+// DOM event to change contact icons and buttons on mouse hover or click
+
+// login button
+let loginColor = document.querySelector('.login-button');
+loginColor.addEventListener('click', e => {
+loginColor.style.backgroundColor = "black";
+loginColor.style.color = white;
+});
+
+let LoginFontColor = document.querySelector('.login-button');
+LoginFontColor.addEventListener('click', e => {
+LoginFontColor.style.color = "white";
+});
+
+// signUp button
+let signUpColor = document.querySelector('.sign-up-btn');
+signUpColor.addEventListener('click', e => {
+signUpColor.style.backgroundColor = "black";
+signUpColor.style.color = white;
+});
+
+let signUpFontColor = document.querySelector('.sign-up-btn');
+signUpFontColor.addEventListener('click', e => {
+signUpFontColor.style.color = "white";
+});
+
+
+// email
 let changeIconColor = document.querySelector('.fa-envelope-open');
 changeIconColor.addEventListener('mouseover', e => {
-changeIconColor.style.color = "orange";
+changeIconColor.style.color = "white";
 });
 
+let regColor = document.querySelector('.fa-envelope-open');
+regColor.addEventListener('mouseout', e => {
+regColor.style.color = "black";
+});
+
+// phone
 let changeIconColor2 = document.querySelector('.fa-mobile-alt');
 changeIconColor2.addEventListener('mouseover', e => {
-changeIconColor2.style.color = "orange";
+changeIconColor2.style.color = "white";
 });
 
+let regColor2 = document.querySelector('.fa-mobile-alt');
+regColor2.addEventListener('mouseout', e => {
+regColor2.style.color = "black";
+});
+
+
+// map
 let changeIconColor3 = document.querySelector('.fa-map-marker-alt');
 changeIconColor3.addEventListener('mouseover', e => {
-changeIconColor3.style.color = "orange";
+changeIconColor3.style.color = "white";
+});
+
+let regColor3 = document.querySelector('.fa-map-marker-alt');
+regColor3.addEventListener('mouseout', e => {
+regColor3.style.color = "black";
 });
